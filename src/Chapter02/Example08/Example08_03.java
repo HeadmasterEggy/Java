d592171e99337437f18dd6b6643f84f0e2d04b9c
Joey <headmastereggy@gmail.com>
2023-04-23 11:05:10 +0800

    add quests

---
+++ src/Chapter02/Example08/Example08_03.java
@@ -0,0 +1,26 @@
+package Chapter02.Example08;
+import java.util.Scanner;
+public class Example08_03 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        System.out.print("输入星号数：");
+        int n = cin.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+
+        for(int i = 1; i <= n; i++){
+            int j;
+            for(j = 1; j <= i; j++){
+                System.out.print("  ");
+            }
+            for(; j <= n; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
--- src/Chapter02/Example08/Example08_04.java
+++ src/Chapter02/Example08/Example08_04.java
@@ -1,19 +1,20 @@
 package Chapter02.Example08;
+
 import java.util.Scanner;
-public class Example08 {
+
+public class Example08_04 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         System.out.print("输入星号数：");
         int n = cin.nextInt();
         for(int i = 1; i <= n; i++){
-            int j;
-            for(j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+            for(int j = 1; j <= n - i; j++){
+                System.out.print("  ");
             }
-            for(int k = j; k <= n; k++){
-                System.out.print("*");
+            for(int k = 1; k <= 2 * i - 1; k++){
+                System.out.print("* ");
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
 }
