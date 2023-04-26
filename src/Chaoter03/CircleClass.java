dc3b782a38c8d08007fcdbd7df6843464f6366ac
Joey <headmastereggy@gmail.com>
2023-04-26 18:09:49 +0800

    add new

--- src/Chaoter03/CircleClass.java
+++ src/Chaoter03/CircleClass.java
@@ -1,14 +1,14 @@
-package Chaoter03.Example01;
+package Chaoter03;
 /*
  * 首先定义一个计算二维坐标系中圆面积的类circleClass，要求类中有一个定义圆心座标,圆上一点座标的构造方法，以及一个通过圆上一点座标与圆心座标计算圆面积的方法area。
  * 然后，通过上述类生成两个圆对象circle1、circle2进行测试：
  * 一个圆心、圆上一点座标分别为(0,0)、(8.5,9)，另一个圆心、圆上一点座标分别为(2,3.5)、(9,6)，并分别显示各自面积。Math.pow(x底数,y几次方) ,x 的 y 次幂
  */
 public class CircleClass {
-    private double centerX;  // 圆心X坐标
-    private double centerY;  // 圆心Y坐标
-    private double pointX;   // 圆上一点X坐标
-    private double pointY;   // 圆上一点Y坐标
+    private final double centerX;  // 圆心X坐标
+    private final double centerY;  // 圆心Y坐标
+    private final double pointX;   // 圆上一点X坐标
+    private final double pointY;   // 圆上一点Y坐标
 
     public CircleClass(double centerX, double centerY, double pointX, double pointY) {
         this.centerX = centerX;
