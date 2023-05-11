2e27a3d9943884c35b9ffdb614de8e5c897c9c69
Joey <headmastereggy@gmail.com>
2023-05-11 08:22:29 +0800

    changes

--- src/Chapter05/MapExample.java
+++ src/Chapter05/MapExample.java
@@ -1,23 +1,20 @@
 package Chapter05;
-import java.util.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * 请用Map实现获取字符串“bwaerbctyxbacecrtdcvr”中每一个字母出现的次数。要求结果格式：a(2)b(3)c(4)…..
  */
 public class MapExample {
     public static void main(String[] args) {
         String s = "bwaerbctyxbacecrtdcvr";
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!map.containsKey(c)) {
-                map.put(c, 1);
-            } else {
-                int count = map.get(c);
-                map.put(c, count + 1);
-            }
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         StringBuilder result = new StringBuilder();
-        for (char c : map.keySet()) {
+        for(char c : map.keySet()){
             result.append(c).append("(").append(map.get(c)).append(")");
         }
         System.out.println(result);
---
+++ src/test4.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+//bwaerbctyxbacecrtdcvr
+public class test4 {
+
+}
