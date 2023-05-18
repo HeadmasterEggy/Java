8428106d4ba8c569ae0824c44e14192f256938fb
Joey <headmastereggy@gmail.com>
2023-05-18 08:41:44 +0800

    changes

--- src/Chapter03/CircleClass.java
+++ src/Chapter03/CircleClass.java
@@ -5,31 +5,36 @@ package Chapter03;
  * 一个圆心、圆上一点座标分别为(0,0)、(8.5,9)，另一个圆心、圆上一点座标分别为(2,3.5)、(9,6)，并分别显示各自面积。Math.pow(x底数,y几次方) ,x 的 y 次幂
  */
 public class CircleClass {
-    private final double centerX;  // 圆心X坐标
-    private final double centerY;  // 圆心Y坐标
-    private final double pointX;   // 圆上一点X坐标
-    private final double pointY;   // 圆上一点Y坐标
-
-    public CircleClass(double centerX, double centerY, double pointX, double pointY) {
-        this.centerX = centerX;
-        this.centerY = centerY;
-        this.pointX = pointX;
-        this.pointY = pointY;
+    final static double PI = 3.14;
+    Points p1, p2;
+    CircleClass(Points p1, Points p2){
+        this.p1 = p1;
+        this.p2 = p2;
     }
 
-    public double area() {
-        double r = Math.sqrt(Math.pow(pointX - centerX, 2) + Math.pow(pointY - centerY, 2)); // 计算半径
-        return Math.PI * Math.pow(r, 2); // 计算面积
+    void area(){
+        System.out.println("面积：" + PI * (Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2)));
     }
-
     public static void main(String[] args) {
-        // 创建两个圆对象
-        CircleClass circle1 = new CircleClass(0, 0, 8.5, 9);
-        CircleClass circle2 = new CircleClass(2, 3.5, 9, 6);
+        Points q1, q2, q3, q4;
+        q1 = new Points(0, 0);
+        q2 = new Points(8.5, 9);
 
-        // 显示各自面积
-        System.out.println("圆1的面积：" + circle1.area());
-        System.out.println("圆2的面积：" + circle2.area());
+        q3 = new Points(2, 3.5);
+        q4 = new Points(9, 6);
+
+        CircleClass circle1 = new CircleClass(q1, q2);
+        circle1.area();
+
+        CircleClass circle2 = new CircleClass(q3, q4);
+        circle2.area();
     }
 }
 
+class Points{
+    double x, y;
+    public Points(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/test4.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
-//bwaerbctyxbacecrtdcvr
-public class test4 {
-
-}
