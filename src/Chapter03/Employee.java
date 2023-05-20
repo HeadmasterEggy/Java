f7a9b3d8fedcaeae017fe8b0e55b5b7673b2ec1c
Joey <headmastereggy@gmail.com>
2023-05-20 14:06:53 +0800

    modified:   src/Chapter03/Employee.java

--- src/Chapter03/Employee.java
+++ src/Chapter03/Employee.java
@@ -8,91 +8,84 @@ package Chapter03;
  */
 
 public class Employee {
-    private final String name;
-    protected int salary;
+    public String name;
+    public int salary;
+    public String description;
 
-    public Employee(String name, int salary){
+    public Employee(String name, String description, int salary) {
         this.name = name;
         this.salary = salary;
+        this.description = description;
     }
 
-    public double getYearSalary(){
-        return salary * 12;
-    }
-
-    public void getTotal(){
-        System.out.println("姓名：" + name);
-        System.out.println("工资：" + salary);
+    public void print() {
+        System.out.println("姓名: " + name);
+        System.out.println("职业: " + description);
+        System.out.println(("工资: " + salary));
     }
 }
 
-class Worker extends Employee{
-    public Worker(String name, int salary) {
-        super(name, salary);
+class Worker extends Employee {
+    public Worker(String name, String description, int salary) {
+        super(name, description, salary);
     }
 }
 
-class Waiter extends Employee{
-    public Waiter(String name, int salary){
-        super(name, salary);
-    }
-}
-
-class Teacher extends Employee {
-    private final int paymentYear;
-    private final int periodYear;
-    public Teacher(String name, int salary, int paymentYear, int periodYear){
-        super(name, salary);
-        this.paymentYear = paymentYear;
-        this.periodYear = periodYear;
-    }
-
-    public double getYearSalary(){
-        return salary * 12 + paymentYear * periodYear;
-    }
-
-    public void getTotal() {
-        super.getTotal();
-        System.out.println("课酬：" + paymentYear * periodYear);
+class Waiter extends Employee {
+    public Waiter(String name, String description, int salary) {
+        super(name, description, salary);
     }
 }
 
 class Scientist extends Employee {
-    private final int bonus;
+    public int bonus;
 
-    public Scientist(String name, int salary, int bonus) {
-        super(name, salary);
+    public Scientist(String name, String description, int salary, int bonus) {
+        super(name, description, salary);
         this.bonus = bonus;
     }
 
-    public double getYearSalary(){
-        return salary * 12 + bonus;
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("科研激励奖: " + bonus);
+    }
+}
+
+class Teacher extends Employee {
+    public int payment, period;
+
+    public Teacher(String name, String description, int salary, int payment, int period) {
+        super(name, description, salary);
+        this.payment = payment;
+        this.period = period;
     }
 
-    public void getTotal(){
-        super.getTotal();
-        System.out.println("科研激励奖：" + bonus);
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("课酬: " + payment + " 元/节");
+        System.out.println("课时: " + period + " 节/年");
     }
 }
 
 class Test {
     public static void main(String[] args) {
-        Employee employee1 = new Worker("张三", 4000);
-        Employee employee2 = new Waiter("李四", 3500);
-        Employee employee3 = new Teacher("王五", 7000, 100, 200);
-        Employee employee4 = new Scientist("刘六", 7000, 20000);
+        Employee worker = new Worker("张三", "工人", 4000);
+        Employee waiter = new Waiter("李四", "服务员", 3500);
+        Employee teacher = new Teacher("王五", "教师", 5000, 100, 200);
+        Employee scientist = new Scientist("刘六", "科学家", 7000, 20000);
 
-        employee1.getTotal();
+        worker.print();
         System.out.println();
 
-        employee2.getTotal();
+        waiter.print();
         System.out.println();
 
-        employee3.getTotal();
+        teacher.print();
         System.out.println();
 
-        employee4.getTotal();
+        scientist.print();
         System.out.println();
     }
 }
-
