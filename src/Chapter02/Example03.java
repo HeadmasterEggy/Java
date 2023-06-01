92a7748a75c27d4ea3b02cc5597258c9b71598c7
Joey <headmastereggy@gmail.com>
2023-06-01 09:00:37 +0800

    changes

--- src/Chapter02/Example03.java
+++ src/Chapter02/Example03.java
@@ -5,7 +5,7 @@ package Chapter02;
 public class Example03 {
     public static void main(String[] args) {
         System.out.println("输入字符串的个数：" + args.length);
-        for(int i = 1; i <= args.length; i++){
+        for(int i = 0; i < args.length; i++){
             System.out.println("字符串" + i + "的个数为：" + args[i].length());
         }
     }
