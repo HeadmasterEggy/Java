3ea75e62a87fab4e6cdec6df5848e14981924421
Joey <headmastereggy@gmail.com>
2023-06-01 09:32:28 +0800

    changes

--- src/Chapter02/Example06.java
+++ src/Chapter02/Example06.java
@@ -1,4 +1,5 @@
 package Chapter02;
+
 /*
  * 角谷猜想：任何一个正整数n，如果它是偶数则除以2，如果是奇数则乘3加1，这样得到一个新的整数，
  * 如此继续进行上述处理，则最后得到的数一定是1。
@@ -6,22 +7,16 @@ package Chapter02;
  */
 public class Example06 {
     public static void main(String[] args) {
-        int flag = 2, num = 10000;
-        for (int i = 3; i <= num; i++) {
-            int k = i;
-            while (true) {
-                if (k % 2 == 0)
-                    k /= 2;
+        int tmp = 0;
+        for (int n = 3; n <= 10000; n++) {
+            tmp = n;
+            while (tmp != 1) {
+                if (tmp % 2 == 1)
+                    tmp = tmp * 3 + 1;
                 else
-                    k = k * 3 + 1;
-
-                if (k == 1) {
-                    flag++;
-                    break;
-                }
+                    tmp = tmp / 2;
             }
+            System.out.println(n + "符合角谷猜想！");
         }
-        if (flag == num)
-            System.out.println("在3~10000之间的所有正整数都符合上述规则");
     }
 }
