f0ca94bab5b584089451bdc44889039aa52041bb
Joey <headmastereggy@gmail.com>
2023-06-01 09:11:01 +0800

    changes

--- src/Chapter02/Example05.java
+++ src/Chapter02/Example05.java
@@ -6,9 +6,6 @@ public class Example05 {
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-        if(a < b)
-            System.out.println(a + " < " + b);
-        else
-            System.out.println(a + " > " + b);
+        System.out.println(a < b ? (a + " < " + b) : (a + " > " + b));
     }
 }
