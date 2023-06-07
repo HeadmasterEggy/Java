7ad4cb0293ab6a455d05d016bf58d7ba55ac9817
Joey <headmastereggy@gmail.com>
2023-06-07 10:26:29 +0800

    changes

--- src/Chapter04/Example01.java
+++ src/Chapter04/Example01.java
@@ -6,30 +6,19 @@ import java.util.Scanner;
  * 如子字符串“nba”在字符串”asfasfnabaasdfnbasdnbasnbasdnbadfasdf”中出现的次数和出现的位置。
  */
 public class Example01 {
-     public static void main(String[] args) {
-
-    Scanner input = new Scanner(System.in);
-
-    // 输入字符串
-    System.out.print("请输入一个字符串: ");
-    String str = input.nextLine().trim();
-
-    // 输入字符子串
-    System.out.print("请输入要统计的字符子串: ");
-    String subStr = input.nextLine().trim();
-
-    // 统计字符子串出现的次数和位置
-    int count = 0;
-    int index = str.indexOf(subStr);
-    while (index >= 0) {
-      count++;
-      System.out.printf("字符子串出现在位置 %d%n", index);
-      index = str.indexOf(subStr, index + 1);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String subStr = input.nextLine();
+        String str = input.nextLine();
+        int sum = 0;
+        int count = 0;
+        int index = 0;
+        int n = 0;
+        while ((index = str.indexOf(subStr, index)) != -1) {
+            count++;
+            index += subStr.length();
+            System.out.println("第" + ++n + "次出现的位置：" + index);
+        }
+        System.out.println("字符串出现" + n + "次");
     }
-
-    // 输出结果
-    System.out.printf("共计出现了 %d 次", count);
-
-    input.close();
-  }
 }
--- src/Rectangle.java
+++
@@ -1,6 +0,0 @@
-/*
- * 在biology包中的animal包中有human类,它具有name,height,weight的属性,还具有eat(),sleep()和work()的行为,
- * 在biology包中的plant包中有flower类,它具有name,color,smell的属性,还具有drink()和blossom()的行为.
- * 现在在一个school包中的garden包中一个张三的人,他是一个human类的对象,种植的rose是一个flower类对象,编程实现并测试各自的方法.
- */
-
