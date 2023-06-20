1ff5dff7f6be0a799d5de31a80637d1754cc8bd5
Joey <headmastereggy@gmail.com>
2023-06-20 18:44:04 +0800

    changes

--- src/Chapter03/CircleClass.java
+++ src/Chapter03/CircleClass.java
@@ -5,15 +5,13 @@ package Chapter03;
  * 一个圆心、圆上一点座标分别为(0,0)、(8.5,9)，另一个圆心、圆上一点座标分别为(2,3.5)、(9,6)，并分别显示各自面积。Math.pow(x底数,y几次方) ,x 的 y 次幂
  */
 public class CircleClass {
-    final static double PI = 3.14;
     Points p1, p2;
     CircleClass(Points p1, Points p2){
         this.p1 = p1;
         this.p2 = p2;
     }
-
     void area(){
-        System.out.println("面积：" + PI * (Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2)));
+        System.out.println("面积：" + Math.PI * (Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2)));
     }
     public static void main(String[] args) {
         Points q1, q2, q3, q4;
